d33b7c291f19640cd2c297c5f12c4abb3c376ac7
Nick <nfuller52@gmail.com>
2018-04-04 21:31:12 -1000

    add title to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,10 @@
-Dir[Rails.root.join("db/seeds/**/*_seed.rb")].each { |f| require f }
+unless Rails.env.production?
+  title = "SEEDING #{Rails.env.upcase}"
+  line_length = 79
+  padding = (line_length - 8 - title.length) / 2
+  printf(("#" * line_length) + "\n")
+  printf("####" + (" " * padding) + "%s" + (" " * padding) + "####\n", title)
+  printf(("#" * line_length) + "\n\n")
+
+  Dir[Rails.root.join("db/seeds/**/*_seed.rb")].each { |f| require f }
+end
